5809c484610d815cc3ea06ff09040d0ef70fa046
Prashant6068 <mishra.prashant@neosoftmail.com>
2022-02-28 18:28:40 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <task v-bind:tasks="tasks"></task>
+  </div>
+</template>
+
+<script>
+import Task from "./components/Task";
+
+export default {
+  name: "App",
+  components: {
+    Task,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Learn Vue JS",
+          completed: false,
+        },
+       
+
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+
+
+
+</style>
